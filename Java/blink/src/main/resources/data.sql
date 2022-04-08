c40933f76f9db157475f201484fa64b3ac8fe987
Rippah <dzoni.pantic@hotmail.com>
2022-04-08 18:30:02 +0200

    data.sql

--- Java/blink/src/main/resources/data.sql
+++ Java/blink/src/main/resources/data.sql
@@ -1,4 +1,13 @@
-INSERT INTO KORISNICI (korisnickoIme, lozinka) VALUES
-    ('Rippah', 'rippah'),
-    ('Admin', 'admin'),
-    ('Pass', 'pass');
+DROP TABLE IF EXISTS TBL_KORISNICI;
+
+CREATE TABLE TBL_KORISNICI (
+                               id INT AUTO_INCREMENT PRIMARY KEY,
+                               korisnickoIme VARCHAR(250) NOT NULL,
+                               lozinka VARCHAR(250) NOT NULL,
+                               email VARCHAR(250) DEFAULT NULL
+);
+
+INSERT INTO TBL_KORISNICI (korisnickoIme, lozinka, email) VALUES
+    ('Rippah', 'rippah', 'd@gmail.com'),
+    ('Admin', 'admin', 'g@gmail.com'),
+    ('Pass', 'pass', 'p@gmail.com');
