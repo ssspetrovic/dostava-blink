6651bc253cf603302e40c76d4259ed30e709aefa
Srdjan Petrovic <s.petrovic852@gmail.com>
2022-04-09 17:53:28 +0200

    java css html
    prepravljen link u pop-up, popravljena velicina na malim rezolucijama, popravljen fokus pop-upa

--- Front-end/script.js
+++ Front-end/script.js
@@ -1,18 +1,25 @@
 const btn = document.querySelector('#zapocni');
 const close_btn = document.querySelector('#close-button')
 const popUp = document.querySelector('.pop-up');
-const navbar =  document.querySelector('.navbar')
+const navbar =  document.querySelector('.navbar');
+const menu = document.querySelector('.menu-bar');
 
 btn.addEventListener('click', () => {
     popUp.classList.add('pop-up__toggle');
     document.body.style.background = 'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(images/background-pattern.png)';
     navbar.style.backgroundColor = '#4c3914';
+    navbar.style.pointerEvents = 'none';
+    navbar.style.pointerEvents = 'none';
+    btn.style.color = '#4c4c4c';
 });
 
 close_btn.addEventListener('click', () => {
+    navbar.style.pointerEvents = '';
+    navbar.style.pointerEvents = '';
     document.body.style.background = '';
     navbar.style.backgroundColor = '';
     popUp.classList.remove('pop-up__toggle');
+    btn.style.color = '';
 });
 
 // background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(https://images.unsplash.com/photo-1614030424754-24d0eebd46b2);
